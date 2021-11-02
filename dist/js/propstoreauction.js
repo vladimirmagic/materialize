f0f40187d887acf0f38a45375f5cb41558d25f1a
Alexey Fominykh <alexey.fominykh@mail.magicwebsolutions.co.uk>
2021-11-02 11:54:31 +0400

    fix product closed

--- dist/js/propstoreauction.js
+++ dist/js/propstoreauction.js
@@ -293,10 +293,13 @@ document.addEventListener('DOMContentLoaded', () => {
 			$win = $('#lac28, #oai21');
 			if ($win.length) {
 				$winVal = $win.find('span');
-				$win.find('span').remove();
-				$winVal.append(' ').append($('.biddingHistoryLink'));
-				$lineWin = $detailsLine.clone();
-				$lineWin.html($win.text() + ' <strong>' + $winVal.html() + '</strong>');
+                $lineWin = $detailsLine.clone();
+				$lineWin.html($win.text());
+                if ($winVal.length) {
+                    $win.find('span').remove();
+				    $winVal.append(' ').append($('.biddingHistoryLink'));
+                    $lineWin.html($lineWin.html() + ' <strong>' + $winVal.html() + '</strong>');
+                }
 				$details.append($lineWin);
 			}
 
